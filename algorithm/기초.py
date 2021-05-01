be8181734615e6f993464d91f12a48062a70e13e
Doosies <song961003@gmail.com>
2021-05-01 13:18:27 +0900

    A + B - 3

--- algorithm/기초.py
+++ algorithm/기초.py
@@ -1,5 +1,9 @@
 n = int(input())
+add_list = []
 # m = int(input())
 # h, m = map(int, input().split())
-for i in range(1,10):
-    print(f'{n} * {i} = {n*i}')
+for i in range(n):
+    a, b = map(int, input().split())
+    add_list.append(a+b)
+for v in add_list:
+    print(v)
