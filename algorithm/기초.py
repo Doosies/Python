86c91df9afe649da31c2b0bbe53de10ff31f77e7
Doosies <song961003@gmail.com>
2021-05-01 13:14:48 +0900

    구구단

--- algorithm/기초.py
+++ algorithm/기초.py
@@ -1,9 +1,5 @@
-# h = int(input())
+n = int(input())
 # m = int(input())
-h, m = map(int, input().split())
-t = (h*60) + m - 45
-h2 = t // 60
-m2 = t % 60
-if h2 < 0:
-    h2 += 24
-print(h2, m2)
+# h, m = map(int, input().split())
+for i in range(1,10):
+    print(f'{n} * {i} = {n*i}')
