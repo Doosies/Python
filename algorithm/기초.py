7f943c3c44c1d17a34f7c0d99741cd177587d6e5
Doosies <song961003@gmail.com>
2021-05-01 13:13:15 +0900

    알람시계

--- algorithm/기초.py
+++ algorithm/기초.py
@@ -1,13 +1,9 @@
-a = int(input())
-b = int(input())
-
-if a > 0:
-    if b > 0:
-        print("1")
-    else:
-        print("4")
-else:
-    if b > 0:
-        print("2")
-    else:
-        print("3")
+# h = int(input())
+# m = int(input())
+h, m = map(int, input().split())
+t = (h*60) + m - 45
+h2 = t // 60
+m2 = t % 60
+if h2 < 0:
+    h2 += 24
+print(h2, m2)
