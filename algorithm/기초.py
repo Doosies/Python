bfd396415bfcea4c631fe2b5950a88a43d3ccf57
Doosies <song961003@gmail.com>
2021-05-01 13:38:16 +0900

    별찍기 - 1

--- algorithm/기초.py
+++ algorithm/기초.py
@@ -1,9 +1,7 @@
 n = int(input())
-nl = []
+
 # m = int(input())
 # h, m = map(int, input().split())
+
 for i in range(n):
-    a, b = map(int, input().split())
-    nl.append([a,b])
-for i,v in enumerate(nl):
-    print(f'Case #{i+1}: {v[0]} + {v[1]} = {v[0]+v[1]}')
+    print('*'*(i+1))
