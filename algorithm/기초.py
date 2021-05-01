dc8faf21c293a3bd6fa72a6100d64de8e14d6ff2
Doosies <song961003@gmail.com>
2021-05-01 13:29:43 +0900

    n찍기

--- algorithm/기초.py
+++ algorithm/기초.py
@@ -1,11 +1,5 @@
-import sys
 n = int(input())
-add_list = []
 # m = int(input())
 # h, m = map(int, input().split())
-for i in range(n):
-    a, b = map(int, sys.stdin.readline().split())
-    add_list.append(a+b)
-for v in add_list:
-    print(v)
-
+for i in range(1, n+1):
+    print(i)
