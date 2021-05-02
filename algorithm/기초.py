9d651483471217ec78339edc9bcee36be3c05822
Doosies <song961003@gmail.com>
2021-05-02 22:40:00 +0900

    수정중2

--- algorithm/기초.py
+++ algorithm/기초.py
@@ -2,42 +2,48 @@ import random
 q = 3
 q2 = 1
 qq = [
-    [0.5,   0.5,    0],
-    [0.5,   0,      0.5], 
-    [0,     0.5,    0.5]
+    [1,   0,    0],
+    [0,   0,    1], 
+    [0,   0,    1]
     ]
-
-def get_percentage_list(n):
-    percentage_list = []
-    for i in range(n):
+n, k = [q,q2]
+users_percentage_list = []
+for i in range(n):
         # a, b, c = map(float, input().split())
         a, b, c = [qq[i][0],qq[i][1],qq[i][2]]
-        percentage =sorted({'a':a, 'b':b, 'c':c}.items(), key=lambda x: x[1])
-        percentage_list.append(percentage)
-    return percentage_list
+        user_action_percentage = {'a':a, 'b':b, 'c':c}
+        percentage_actions = sorted(user_action_percentage.items(), key=lambda x: x[1])
+        user_dict = {'user':i, 'action':percentage_actions}
+        users_percentage_list.append(user_dict)
+
+# def get_users_percentage_list():
+#     percentage_list = []
+    
+#     return percentage_list
 
     
-# 유저 한명의 행동 정하기
+# 모든 유저의 액션 설정
 def get_users_action(p_list, n):
     users_actions = {'a':[], 'b':[], 'c':[]}
-    # 모든 유저의 액션 설정
     while True:
         for i in range(n):
             add_user_rand = 0.0
             # 유저 한명의 액션 설정
             for j,_ in enumerate(p_list):
                 rand = round(random.random(),3)
-                add_user_rand += p_list[i][j][1]
+                add_user_rand += p_list[i]['action'][j][1]
                 # 랜덤값 찾으면
                 if rand < add_user_rand:
-                    user_action = p_list[i][j][0]
+                    user_action = p_list[i]['action'][j][0]
                     user = {'user':i, 'action':user_action}
                     users_actions[user_action].append(user)
+                    print(users_actions)
                     break
         if not (users_actions['a'] and users_actions['b'] and users_actions['c']):
             break
+        else:
+            print(users_actions, end='\n')
     return users_actions
-
 
 # 승자 한명 반환
 def get_winner_loser(users_dict):
@@ -55,22 +61,25 @@ def get_winner_loser(users_dict):
     else: 
         return None
 
-def dfs_win(winners,n):
-    percentage_list = get_percentage_list(n)
-    actions = get_users_action(percentage_list, len(winners) )
-    winners, losers = get_winner_loser(actions)
-
-    # actions = 
-    # winner, loser = get_winner_loser(winners)
+def dfs_win(actions):
     
-    
-def main():
+    winner, loser = get_winner_loser(actions)
+    if len(winner) > 1:
+        print("2명이상이 이김")
+    if len(loser) > 1:
+        print("2명 이상이 짐")
+    print(winner)
+    print(loser)
+# def main():
     # n = 참가자, k = 등수
     # n, k = map(int, input().split())
-    n, k = [q,q2]
-    percentage_list = get_percentage_list(n)
-    actions = get_users_action(percentage_list, n )
-    winners, losers = get_winner_loser(actions)
-    dfs_win(winners,n)
-    
-main()
+    # percentage_list = get_percentage_list(n)
+    # actions = get_users_action(percentage_list, n )
+    # winners, losers = get_winner_loser(actions)
+
+actions = get_users_action(users_percentage_list, n)
+dfs_win(actions)
+# percentage_list = get_users_percentage_list()
+# print(users_percentage_list)
+# dfs_win(winners,n)
+# main()
