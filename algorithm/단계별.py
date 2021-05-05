92fef6eabbeb121f5747079b1dd2689a98c941ac
Doosies <song961003@gmail.com>
2021-05-05 16:27:47 +0900

    한수

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,11 +1,17 @@
-ll = set(range(1,10001))
-rm = set()
+n = int(input())
+cnt = 0
 
-for i in ll:
-    add = int(i)
-    for j in str(i):
-        add += int(j)
-    rm.add(add)
-result = ll - rm
-for num in sorted(result):
-    print(num)
+def isSeq(num):
+    num = (str(num))
+    plus = set()
+    
+    for j in range(len(num) -1 ,0,-1):
+        r = int(num[j]) - int(num[j-1])
+        plus.add(r)
+    return True if len(plus) == 1 else False
+
+for i in range(1,n+1):
+    if isSeq(i) == True or i < 100:
+        cnt +=1
+        print(i)
+print(cnt)
