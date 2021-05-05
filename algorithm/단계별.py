b7a8584d803d2bcdc4ac812676bb7a8c16ac5127
Doosies <song961003@gmail.com>
2021-05-05 14:55:04 +0900

    평균

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,2 +1,5 @@
-input_list = set(int(input())%42 for _ in range(10))
-print(len(input_list))
+n = int(input())
+subject_list = list(map(int, input().split()))
+max_num = max(subject_list)
+new_subject_list = [val/max_num*100 for val in subject_list]
+print(sum(new_subject_list) / len(new_subject_list))
