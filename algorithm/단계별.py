d7abcb4055c507bfd4529d2bd65670f844c11534
Doosies <song961003@gmail.com>
2021-05-05 16:36:25 +0900

    아스키코드

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,16 +1,2 @@
-n = int(input())
-cnt = 0
-
-def isSeq(num):
-    num = (str(num))
-    plus = set()
-    
-    for j in range(len(num) -1 ,0,-1):
-        r = int(num[j]) - int(num[j-1])
-        plus.add(r)
-    return True if len(plus) == 1 else False
-
-for i in range(1,n+1):
-    if isSeq(i) == True or i < 100:
-        cnt +=1
-print(cnt)
+n = input()
+print(ord(n))
