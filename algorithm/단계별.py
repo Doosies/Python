bb681fb4011505acd32198ef59a09a9f51ce5915
Doosies <song961003@gmail.com>
2021-05-05 15:30:56 +0900

    셀프넘버

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,7 +1,9 @@
-C = int(input())
-cases = list(list(map(int,input().split())) for _ in range(C))
-for case in cases:
-    avg = sum(case[1:]) / case[0]
-    bigger = [score for score in case[1:] if score > avg]
-    percentage = len(bigger) / case[0] * 100
-    print(f'{percentage:0.3f}%')
+ll = []
+for i in range(1,10001,1):
+    add = int(i)
+    for j in str(i):
+        add += int(j)
+    ll.append(add)
+for i in range(1,10001,1):
+    if i not in ll:
+        print(i)
