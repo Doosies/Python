c28a3ea8c5eec35c066bcdc3bba8b12d50a17949
Doosies <song961003@gmail.com>
2021-05-05 15:35:26 +0900

    셀프넘버 수정

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,9 +1,11 @@
-ll = []
-for i in range(1,10001,1):
+ll = set(range(1,10001))
+rm = set()
+
+for i in ll:
     add = int(i)
     for j in str(i):
         add += int(j)
-    ll.append(add)
-for i in range(1,10001,1):
-    if i not in ll:
-        print(i)
+    rm.add(add)
+result = ll - rm
+for num in sorted(result):
+    print(num)
