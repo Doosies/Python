31962a4d2c210ff996dd367fe0f3f9462047715a
Doosies <song961003@gmail.com>
2021-05-06 12:23:22 +0900

    상수

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,2 +1,6 @@
-n = input().rstrip(' ').lstrip(' ').split()
-print(len(n))
+n = input().split()
+n2 = [''.join(list(reversed(n[i])))
+        for i in range(2) 
+     ]
+n3 = [int(val) for val in n2]
+print(max(n3))
