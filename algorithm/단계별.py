8094cab696e369608456233d6206b5be1bf6cf6e
Doosies <song961003@gmail.com>
2021-05-06 15:30:51 +0900

    그룹 단어 체커

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,21 +1,20 @@
-s_list = ['c-', 'd-', 'dz=', 's=', 'z=', 'c=', 'lj', 'nj']
-n = input()
-i = len(n)-1
-sum = 0
+n = int(input())
+st_list = list(input() for _ in range(n))
+cnt = 0
 
-def is_in(string):
-    for check in s_list:
-        if string == check or string[1:] == check:
-            if len(check) == 3:
-                return 3
-            else:
-                return 2
-    return 1
-
-while i>= 0: 
-    string = n[i-2:i+1] if i-2>= 0 else n[i-1:i+1]
-    isn = is_in(string)
-    i -= isn
-    sum += 1
+for string in st_list:
+    no = False
+    for i,one in enumerate(string):
+        count = string[:i].count(one)
+        if i > 0 and count >= 1:
+            if string[i-1] != one:
+                no = True
+                break
+    if no == False:
+        cnt += 1
+print(cnt)
     
-print(sum)
+
+    
+
+
