a6bb8740b10d0611b32ed5165f2936d69472a242
Doosies <song961003@gmail.com>
2021-05-06 14:37:42 +0900

    크로아티아 알파벳

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,13 +1,21 @@
+s_list = ['c-', 'd-', 'dz=', 's=', 'z=', 'c=', 'lj', 'nj']
+n = input()
+i = len(n)-1
 sum = 0
-n = input().upper()
-l = [[],[],[],
-     ['A','B','C'],['D','E','F'],['G','H','I'],
-     ['J','K','L'],['M','N','O'],['P','Q','R','S'],
-     ['T','U','V'],['W','X','Y','Z']
-    ]
-for string in n:
-    for i,v in enumerate(l):
-        if string in v:
-            sum += i
-            break
+
+def is_in(string):
+    for check in s_list:
+        if string == check or string[1:] == check:
+            if len(check) == 3:
+                return 3
+            else:
+                return 2
+    return 1
+
+while i>= 0: 
+    string = n[i-2:i+1] if i-2>= 0 else n[i-1:i+1]
+    isn = is_in(string)
+    i -= isn
+    sum += 1
+    
 print(sum)
