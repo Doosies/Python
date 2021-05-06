45d5ef10d8bdaf91c38d6fa9938dbb1a350afb34
Doosies <song961003@gmail.com>
2021-05-06 23:37:52 +0900

    달팽이는 올라가고 싶다

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,22 +1,18 @@
-# 1 1                             1   1     2
-# 1 2 / 2 1                       2   3     1
-# 3 1 / 2 2 / 1 3                 3   6     2
-# 1 4 / 2 3 / 3 2 / 4 1           4   10    1
-n = int(input())
-max_num = 0
-line = 0
-# result = [0,0]
+import math
 
-while n > max_num:
-    line += 1
-    max_num += line
-now_col = max_num - n 
+a, b, c = map(int, input().split())
 
-if line % 2 == 0:
-    top = line - now_col
-    bot = now_col + 1
-else:
-    top = now_col + 1
-    bot = line - now_col
+# 10 - 2 + 1
+# a,b,c = [5,1,6]
+print(math.ceil((c-a)/(a-b))+1)
+# now = 0
+# day = 0
 
-print(f'{top}/{bot}')   
+# while True:
+#     day += 1
+#     now += a
+#     if now >= c:
+#         break
+#     now -= b
+# print(day)
+
