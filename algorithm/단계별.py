e2969e2e736a944ccd2028e150d77be2e76b08a7
Doosies <song961003@gmail.com>
2021-05-06 19:49:59 +0900

    변경

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -3,30 +3,20 @@
 # 3 1 / 2 2 / 1 3                 3   6     2
 # 1 4 / 2 3 / 3 2 / 4 1           4   10    1
 n = int(input())
-first_n = n
-row_num, cnt = 1, 0
-last_num = 0
-result = [0,0]
+max_num = 0
+line = 0
+# result = [0,0]
 
-while True:
-    n -= row_num
-    if n <= 0:
-        break
-    last_num += row_num
-    row_num += 1
-last_num = first_n -last_num
-    
-a = list(range(1,row_num+1))
-b = list(range(row_num, 0, -1))
-    
-for j,k in zip(a,b):
-    if row_num%2 == 0:
-        result = [j,k]
-    else:
-        result = [k,j]
+while n > max_num:
+    line += 1
+    max_num += line
+now_col = max_num - n 
 
-    cnt += 1
-    if cnt == last_num:
-        break
+if line % 2 == 0:
+    top = line - now_col
+    bot = now_col + 1
+else:
+    top = now_col + 1
+    bot = line - now_col
 
-print(f'{result[0]}/{result[1]}')
+print(f'{top}/{bot}')   
