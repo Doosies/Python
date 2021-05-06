e2993042396d9be1c252283ac57db40953aa868a
Doosies <song961003@gmail.com>
2021-05-06 12:09:29 +0900

    단어의 개수

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,11 +1,2 @@
-n = input().upper()
-m = {}
-for char in n:
-    m[char] = n.count(char)
-    n.strip(char)
-    
-if len(set(m.values())) != len(m.values()):
-    print("?")
-else:
-    sort = sorted(m.items(), key=lambda x: x[1])
-    print(sort[-1][0])
+n = input().rstrip(' ').lstrip(' ').split()
+print(len(n))
