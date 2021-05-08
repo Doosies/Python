441d9e6f46cb66cb16b4a5e5cc6f70c42cc019b0
Doosies <song961003@gmail.com>
2021-05-08 19:54:26 +0900

    설탕배달

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,15 +1,13 @@
-# t개의 테스트 데이터
-# h 층수
-# w 방개수, w는 필요없어
-# n 번째 도착 손님
-import math
-t = int(input())
-t2 = [list(map(int,input().split())) for _ in range(t)]
+n = int(input())
+cnt = 0
 
-for h,w,n in t2:
-    result_w = str(math.ceil(n/h))
-    result_h = h if n % h == 0 else n % h
-    if len(result_w) == 1:
-        result_w = '0'+result_w
-    print(f'{result_h}{result_w}')
-
+while True:
+    if n % 5 == 0:
+        cnt += n/5
+        break
+    elif n <= 0:
+        cnt = -1
+        break
+    n -= 3
+    cnt += 1
+print(int(cnt))
