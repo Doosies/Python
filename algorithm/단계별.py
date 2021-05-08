3940f3a6ce17f79fa8ebfb16dcfd4e08c3117ae0
Doosies <song961003@gmail.com>
2021-05-08 19:55:22 +0900

    큰 수 A+B

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,13 +1,2 @@
-n = int(input())
-cnt = 0
-
-while True:
-    if n % 5 == 0:
-        cnt += n/5
-        break
-    elif n <= 0:
-        cnt = -1
-        break
-    n -= 3
-    cnt += 1
-print(int(cnt))
+a, b = map(int,input().split())
+print(a+b)
