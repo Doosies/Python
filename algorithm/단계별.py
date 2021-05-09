9e65eebb1e9cc8931f335136f6fa9ddd9ea98f49
Doosies <song961003@gmail.com>
2021-05-09 21:41:06 +0900

    Fly me to the Alpha Centauri

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,2 +1,18 @@
-a, b = map(int,input().split())
-print(a+b)
+import math
+
+t = int(input())
+points = [list(map(int,input().split()))for _ in range(t)]
+
+for point in points:
+    dist = point[1]-point[0]
+    sqt = round(math.sqrt(dist))
+    cnt = int((sqt * 2) -1)
+    dist -= sqt ** 2
+
+    while dist > 0:
+        if dist - sqt >= 0:
+            dist -= sqt
+            cnt += 1
+        else:
+            sqt -= 1
+    print(cnt)
