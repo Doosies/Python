fa30d4ac3726e56089408b4f001170abb6d67eae
Doosies <song961003@gmail.com>
2021-05-10 11:16:07 +0900

    11

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,2 +1,14 @@
-a, b = map(int,input().split())
-print(a+b)
+from math import sqrt,floor
+n = int(input())
+li = map(int, input().split())
+cnt = 0
+
+for i in li:
+    if i == 1:
+        cnt += 1
+    else:
+        for j in range(2, floor(sqrt(i))+1):
+            if i % j == 0:
+                cnt += 1
+                break
+print(n-cnt)
