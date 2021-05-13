f9c7dad19430773bcfc193e0af265006a30f3020
Doosies <song961003@gmail.com>
2021-05-13 15:45:17 +0900

    직사각형에서 탈출

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,16 +1,5 @@
 import sys
-t = int(sys.stdin.readline())
-inp = [int(sys.stdin.readline()) for _ in range(t)]
-mi = max(inp)
-prime = [True] * mi
-
-for i in range( 2, int(mi**0.5)+1):
-    if prime[i]:
-        for j in range( i*2, mi, i):
-            prime[j] = False
-            
-for number in inp:
-    for i in range(number//2, number):
-        if prime[i] and prime[number-i]:
-            print(number-i, i)
-            break
+x,y,w,h = map(int, sys.stdin.readline().split())
+near_x = w-x if w-x < x else x
+near_y = h-y if h-y < y else y
+print(min([near_x, near_y]))
