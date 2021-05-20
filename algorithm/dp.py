129dfed74d49ce29fc2f52c10e72fb0a4286875a
Doosies <song961003@gmail.com>
2021-05-20 18:21:25 +0900

    #2750, 삽입정렬

--- algorithm/dp.py
+++ algorithm/dp.py
@@ -2,5 +2,9 @@ import sys; read = sys.stdin.readline
 
 n = int(read().rstrip())
 num = [int(read().rstrip()) for _ in range(n)]
-num = sorted(num)
-print("\n".join(map(str,num)))
+
+for i in range(1, len(num)):
+    for j in range(i, 0, -1):
+        if num[j-1] > num[j]:
+            num[j-1], num[j] = num[j], num[j-1]
+print(num)
