05b391c1ff7ce6bd1b15c416a23ba73238dc366d
Doosies <song961003@gmail.com>
2021-05-20 19:57:36 +0900

    #2751, 수 정렬하기2

--- algorithm/dp.py
+++ algorithm/dp.py
@@ -2,9 +2,38 @@ import sys; read = sys.stdin.readline
 
 n = int(read().rstrip())
 num = [int(read().rstrip()) for _ in range(n)]
+# num = [6, 5, 3, 1, 8, 7, 2, 4, 9,10]
 
-for i in range(len(num), 0, -1):
-    for j in range(1, i):
-        if num[j] < num[j-1]:
-            num[j], num[j-1] = num[j-1], num[j]
-print("\n".join(map(str,num)))
+def merge(left, right):
+    sorted_li = []
+    left_idx, right_idx = 0, 0
+    while left_idx < len(left) and right_idx < len(right):
+        if left[left_idx] < right[right_idx]:
+            sorted_li.append(left[left_idx])
+            left_idx += 1
+        else:
+            sorted_li.append(right[right_idx])
+            right_idx += 1
+    while left_idx < len(left):
+        sorted_li.append(left[left_idx])
+        left_idx += 1
+
+    while right_idx < len(right):
+        sorted_li.append(right[right_idx])
+        right_idx += 1
+
+    return sorted_li
+
+def merge_sort(arr):
+    if len(arr) == 1:
+        return arr
+    
+    mid = len(arr)//2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    
+    return merge(left, right)
+    
+
+a = merge_sort(num)
+print("\n".join(map(str,a)))
