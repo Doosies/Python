00177163b56f5a3ab5f1579868000677022acaf7
Doosies <song961003@gmail.com>
2021-05-20 18:25:05 +0900

    #2750, 버블정렬

--- algorithm/dp.py
+++ algorithm/dp.py
@@ -3,8 +3,8 @@ import sys; read = sys.stdin.readline
 n = int(read().rstrip())
 num = [int(read().rstrip()) for _ in range(n)]
 
-for i in range(1, len(num)):
-    for j in range(i, 0, -1):
-        if num[j-1] > num[j]:
-            num[j-1], num[j] = num[j], num[j-1]
-print(num)
+for i in range(len(num), 0, -1):
+    for j in range(1, i):
+        if num[j] < num[j-1]:
+            num[j], num[j-1] = num[j-1], num[j]
+print("\n".join(map(str,num)))
