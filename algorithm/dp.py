be21a92d58cb3884ef48f3687625190d42525ef8
Doosies <song961003@gmail.com>
2021-05-20 18:14:21 +0900

    #2750, 수 정렬하기

--- algorithm/dp.py
+++ algorithm/dp.py
@@ -1,18 +1,6 @@
-import sys
-read = sys.stdin.readline
+import sys; read = sys.stdin.readline
 
-
-gg = list(range(0,9999))
-gg = [str(val).zfill(3) for val in gg]
-rst = []
-for i in gg :
-    for idx in range(len(i)+1):
-        li = list(i)
-        li.insert(idx,'666')
-        rr = int( ''.join(li) )
-        rst.append(rr)
-
-rst = list(set(rst))
-rst.sort()
-N = int(read())
-print(rst[N-1])
+n = int(read().rstrip())
+num = [int(read().rstrip()) for _ in range(n)]
+num = sorted(num)
+print("\n".join(map(str,num)))
