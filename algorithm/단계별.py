f8341b400c16e668b4cf7b59424a162706b9843b
Doosies <song961003@gmail.com>
2021-05-27 17:56:03 +0900

    #15651, product 사용

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,17 +1,6 @@
 import sys; read = sys.stdin.readline
 from itertools import permutations, combinations, product
 
-n, m = map(int, read().split())
-# pool = [str(i) for i in range(1,n+1)]
-# for i in combinations (pool, m):
-#     print(" ".join(i))
-
-def solv(now):
-    if len(now) >= m:
-        print(' '.join(now))
-        return now
-    for i in range(1, n+1):
-        now.append(str(i))
-        solv(now)
-        now.pop()
-solv([])
+n, m = map(int, input().split())
+p = [str(i) for i in range(1,n+1)]
+print("\n".join(map(" ".join, product(p, repeat=m))))
