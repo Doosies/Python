bc294c0b181ad70409c789ae1c45350dea4faa5c
Doosies <song961003@gmail.com>
2021-05-27 17:31:59 +0900

    #15650, N과 M(2)

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,7 +1,16 @@
 import sys; read = sys.stdin.readline
-from itertools import permutations
+from itertools import permutations, combinations
 
-n,m = map(int, read().split())
-p = [str(i) for i in range(1, n+1)]
-for sq in permutations(p,m):
-    print(' '.join(sq))
+
+def solv(now):
+    if len(now) >= m:
+        print(" ".join(map(str,now)))
+        return now
+    for i in range(1, n+1):
+        if i not in now and (len(now) == 0 or i >= now[-1]):
+            now.append(i)
+            solv(now)
+            now.pop()
+
+n, m = map(int, read().split())
+solv([])
