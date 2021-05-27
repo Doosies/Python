4b4c4396206073aed1aef20fece4566fc6c91324
Doosies <song961003@gmail.com>
2021-05-27 13:04:40 +0900

    #2108, Counter 사용한 버전

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,22 +1,11 @@
 import sys; read = sys.stdin.readline
-
+from collections import Counter
 
 n = int(read()) # 입력되는값, 홀수
-
-num_li = [int(read()) for _ in range(n)]
-num_li.sort()
-
-cnt = {v:0 for v in num_li}
-for i in num_li:
-    cnt[i] += 1
-max_key, max_cnt = max(cnt.items(),key=lambda x:x[1])
-maxxxx = list(filter(lambda x: x[1] == max_cnt, cnt.items()))
-if len(maxxxx) > 1:
-    maxxxx = maxxxx[1][0]
-else:
-    maxxxx = maxxxx[0][0]
+num_li = sorted([int(read()) for _ in range(n)])
+cnt = Counter(num_li).most_common(2)
 
 print(round(sum(num_li) / n))
 print(num_li[n//2])
-print(maxxxx)
+print(cnt[-1][0] if cnt[0][1] == cnt[-1][1]  else cnt[0][0])
 print(max(num_li)-min(num_li))
