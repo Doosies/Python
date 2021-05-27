aac5e3801d82661dfd76eb23b9dd82c9b3395005
Doosies <song961003@gmail.com>
2021-05-27 13:33:27 +0900

    #11651, 좌표 정렬하기2

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -3,5 +3,5 @@ import sys; read = sys.stdin.readline
 n = int(read())
 pos = [list(map(int,read().split())) for _ in range(n)]
 
-for i in sorted(pos, key=lambda x: (x[0], x[1])):
+for i in sorted(pos, key=lambda x: (x[1], x[0])):
     print(i[0], i[1])
