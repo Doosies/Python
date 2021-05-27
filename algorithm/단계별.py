87a68e17ff48dedcd354182ccb1b6c9ccf729e2d
Doosies <song961003@gmail.com>
2021-05-27 17:34:07 +0900

    #15650, combinations 사용

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,16 +1,7 @@
 import sys; read = sys.stdin.readline
 from itertools import permutations, combinations
 
-
-def solv(now):
-    if len(now) >= m:
-        print(" ".join(map(str,now)))
-        return now
-    for i in range(1, n+1):
-        if i not in now and (len(now) == 0 or i >= now[-1]):
-            now.append(i)
-            solv(now)
-            now.pop()
-
 n, m = map(int, read().split())
-solv([])
+pool = [str(i) for i in range(1,n+1)]
+for i in combinations (pool, m):
+    print(" ".join(i))
