3ff4e2b1678eb66984c7b069f777a045d2860488
Doosies <song961003@gmail.com>
2021-05-27 15:41:47 +0900

    #18870, 좌표 압축

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,13 +1,7 @@
 import sys; read = sys.stdin.readline
 
 n = int(read())
-info = [[read().split(),i] for i in range(n)]
-# info = [
-#     [[1, 'a'],0],
-#     [[2, 'a'],1],
-#     [[3, 'a'],2],
-#     # [[20, 'Sunyoung'],2],
-# ]
-info = sorted(info, key=lambda x: (int(x[0][0]), int(x[1])))
-for i in info:
-    print(i[0][0], i[0][1])
+num = list(map(int, read().split()))
+num_dict = {v:i for i,v in enumerate(sorted(set(num)))}
+for i in num:
+    print(num_dict[i], end=' ')
