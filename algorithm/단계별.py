b723c1c7e037686dfb286417c4c020bcfbf8f62d
Doosies <song961003@gmail.com>
2021-05-27 17:40:20 +0900

    #15651, N과 M (3)

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,7 +1,17 @@
 import sys; read = sys.stdin.readline
-from itertools import permutations, combinations
+from itertools import permutations, combinations, product
 
 n, m = map(int, read().split())
-pool = [str(i) for i in range(1,n+1)]
-for i in combinations (pool, m):
-    print(" ".join(i))
+# pool = [str(i) for i in range(1,n+1)]
+# for i in combinations (pool, m):
+#     print(" ".join(i))
+
+def solv(now):
+    if len(now) >= m:
+        print(' '.join(now))
+        return now
+    for i in range(1, n+1):
+        now.append(str(i))
+        solv(now)
+        now.pop()
+solv([])
