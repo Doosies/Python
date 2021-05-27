8036785ef2d05f400601a559dfcbebbe652ccbe9
Doosies <song961003@gmail.com>
2021-05-27 12:54:21 +0900

    #2108, 통계학

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,12 +1,22 @@
-import sys
-read = sys.stdin.readline
+import sys; read = sys.stdin.readline
 
-n = int(read().rstrip())
-count_list = [0 for _ in range(10001)]
 
-for i in range(n):
-    count_list[int(read().rstrip())] += 1
-    
-for i in range(1, 10001):
-    for j in range(count_list[i]):
-        print(i)
+n = int(read()) # 입력되는값, 홀수
+
+num_li = [int(read()) for _ in range(n)]
+num_li.sort()
+
+cnt = {v:0 for v in num_li}
+for i in num_li:
+    cnt[i] += 1
+max_key, max_cnt = max(cnt.items(),key=lambda x:x[1])
+maxxxx = list(filter(lambda x: x[1] == max_cnt, cnt.items()))
+if len(maxxxx) > 1:
+    maxxxx = maxxxx[1][0]
+else:
+    maxxxx = maxxxx[0][0]
+
+print(round(sum(num_li) / n))
+print(num_li[n//2])
+print(maxxxx)
+print(max(num_li)-min(num_li))
