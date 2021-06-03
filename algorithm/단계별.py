f35e3d18a5a1fe809827c866d0daf726f82186b9
Doosies <song961003@gmail.com>
2021-06-03 10:34:43 +0900

    #11399, ATM

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,11 +1,11 @@
 import sys; read = sys.stdin.readline
 
 n = int(read().rstrip())
-table = [list(map(int,read().split())) for _ in range(n)]
-start = 0
-cnt = 0
-for i in sorted(table,key=lambda x:(x[1],x[0])):
-    if i[0] >= start:
-        start = i[1]
-        cnt += 1
-print(cnt)
+time = list(map(int, read().split()))
+time.sort()
+result1 = 0
+result2 = []
+for i in time:
+    result1 += i
+    result2.append(result1)
+print(sum(result2))
