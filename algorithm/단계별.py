7cf8a2690d8e7b12f5aafad760db13e6f6cc7c16
Doosies <song961003@gmail.com>
2021-06-03 10:34:25 +0900

    #1931, 회의실 배정

--- algorithm/단계별.py
+++ algorithm/단계별.py
@@ -1,25 +1,11 @@
 import sys; read = sys.stdin.readline
 
-# st = '55-50+40' + '+'
-st = read().rstrip() + '+'
-is_plus = True
-string = ''
-result = 0
-
-for c in st:
-    if c == '-':
-        if is_plus:
-            result += int(string)
-        else:
-            result -= int(string)
-        is_plus = False
-        string = ''
-    elif c == '+':
-        if is_plus:
-            result += int(string)
-        else:
-            result -= int(string)
-        string = ''
-    else:
-        string += c
-print(result)
+n = int(read().rstrip())
+table = [list(map(int,read().split())) for _ in range(n)]
+start = 0
+cnt = 0
+for i in sorted(table,key=lambda x:(x[1],x[0])):
+    if i[0] >= start:
+        start = i[1]
+        cnt += 1
+print(cnt)
